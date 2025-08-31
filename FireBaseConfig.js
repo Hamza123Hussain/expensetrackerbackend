5f77b1f9d3e061278cdbc6cde8475ff6f0382044
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-08-31 15:27:39 +0500

    Update FireBaseConfig.js

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -1,21 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import dotenv from 'dotenv'
+dotenv.config()
 const firebaseConfig = {
-  apiKey: 'AIzaSyDLffkYAqPHO6cwDk9D2O01lGryMSDvi_Y',
-  authDomain: 'expensetracker-58e8c.firebaseapp.com',
-  projectId: 'expensetracker-58e8c',
-  storageBucket: 'expensetracker-58e8c.firebasestorage.app',
-  messagingSenderId: '998872801446',
-  appId: '1:998872801446:web:e52776e3b2dc07dbb8d467',
-  measurementId: 'G-YN4Z1J3QB3',
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+export const db = getFirestore(app)
+export const auth = getAuth(app)
