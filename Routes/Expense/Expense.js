29f79b6920851078ed5b7b0d2187b67bd8971f53
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-08-31 16:08:40 +0500

    Update Expense.js

--- Routes/Expense/Expense.js
+++ Routes/Expense/Expense.js
@@ -1,7 +1,11 @@
 import { Router } from 'express'
 import { AddExpense } from '../../DB/AddExpense.js'
 import { AddProject } from '../../DB/AddNewProject.js'
+import { GetExpenses } from '../../DB/GetExpenseofProject.js'
+import { GetProjects } from '../../DB/GetProjects.js'
 
 export const expenserouter = Router()
 expenserouter.post('/AddExpense', AddExpense)
 expenserouter.post('/AddProject', AddProject)
+expenserouter.get('/GetExpenses', GetExpenses)
+expenserouter.get('/GetProjects', GetProjects)
