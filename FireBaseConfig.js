2cfe9ee40ba2e23c7a401a3be175148883c30600
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-08-31 16:15:39 +0500

    Update FireBaseConfig.js

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import dotenv from 'dotenv'
+dotenv.config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAT6WX9DKew88MYqs66P9G5Opj_cBjqfr8',
-  authDomain: 'money-maker-7fd5b.firebaseapp.com',
-  databaseURL: 'https://money-maker-7fd5b.firebaseio.com',
-  projectId: 'money-maker-7fd5b',
-  storageBucket: 'money-maker-7fd5b.firebasestorage.app',
-  messagingSenderId: '523283306364',
-  appId: '1:523283306364:web:41c6621ebe4776862436f9',
-  measurementId: 'G-2Q4ZXZGF2C',
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 }
 
 // Initialize Firebase
